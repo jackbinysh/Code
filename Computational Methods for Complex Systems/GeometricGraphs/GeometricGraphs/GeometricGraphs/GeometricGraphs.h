7032b511e9a966f2c72fbbaaf914dd9210a856a9
jackbinysh <jack.binysh@gmail.com>
2015-01-26 18:47:25 +0000

    no message

--- Computational Methods for Complex Systems/GeometricGraphs/GeometricGraphs/GeometricGraphs/GeometricGraphs.h
+++ Computational Methods for Complex Systems/GeometricGraphs/GeometricGraphs/GeometricGraphs/GeometricGraphs.h
@@ -6,9 +6,15 @@ using namespace Eigen;
 
 namespace GeometricGraphs
 {
-	void CreateLaplacian(Matrix4d& xInputMatrix, double dDistance, std::mt19937& xEngine, std::uniform_real_distribution<double>& xDistribution);
-	// function which implements to QR algorithm on a tridiagonal matrix to find eigenvalues.
-	void QRAlgorithm(Matrix4d& xInputMatrix, int iNumruns);
+	typedef  Matrix<double, 4, 4> MyMatrix;
+	typedef Matrix<double, 4, 1> MyVector;
+
+	void CreateLaplacian(MyMatrix& xInputMatrix, double dDistance, std::mt19937& xEngine, std::uniform_real_distribution<double>& xDistribution);
+
+	// a function which takes a symettric matrix and tridiagonalises it
+	void TriDiag(MyMatrix& xInputMatrix);
+	// function which implements to QR algorithm on a tridiagonal input matrix to find eigenvalues.
+	void QRAlgorithm(MyMatrix& xInputMatrix, int iNumiterations, double dEpsilon);
 	//function which encodes the distance between two nodes, with a period given by Mod
 	double Distance(double dX, double dY, double Mod);
 }
